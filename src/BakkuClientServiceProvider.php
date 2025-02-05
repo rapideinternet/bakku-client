797da0d762b51fc04f7058f192c5b07caf69b893
Max van der Werf <max.vanderwerf@rapide.software>
2025-02-05 15:56:34 +0100

    fix: didnt instantiate transformer in service provider

--- src/BakkuClientServiceProvider.php
+++ src/BakkuClientServiceProvider.php
@@ -8,14 +8,19 @@ use RapideSoftware\BakkuClient\Contracts\CacheInterface;
 use RapideSoftware\BakkuClient\Services\BakkuClientService;
 use RapideSoftware\BakkuClient\Services\BakkuClientCacheService;
 use RapideSoftware\BakkuClient\Services\HttpClientService;
+use RapideSoftware\BakkuClient\Transformers\ApiResponseTransformer;
 
 class BakkuClientServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(BakkuClientInterface::class, BakkuClientService::class);
-        $this->app->bind(CacheInterface::class, BakkuClientCacheService::class);
-        $this->app->bind(HttpClientService::class, HttpClientService::class);
+        $this->app->singleton(ApiResponseTransformer::class, function ($app) {
+            return new ApiResponseTransformer();
+        });
+
+        $this->app->singleton(BakkuClientInterface::class, BakkuClientService::class);
+        $this->app->singleton(CacheInterface::class, BakkuClientCacheService::class);
+        $this->app->singleton(HttpClientService::class, HttpClientService::class);
 
         $this->mergeConfigFrom(__DIR__.'/Config/bakkuclient.php', 'bakkuclient');
     }
